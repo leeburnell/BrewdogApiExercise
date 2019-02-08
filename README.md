9df125132bc9997dde1e5c8759c0c184271f204d
leeburnell <iambeeroy@gmail.com>
2019-02-08 21:32:18 +0000

    Bug Fixing
    Fixed the inline JS issue and added more info into the readme file

--- README.md
+++ README.md
@@ -1,10 +1,1 @@
-This project uses ES6 and the BrewdogAPI to gather a list of all beers and display them using CSS Grid.
-
-Technologies used:
-
-HTML, CSS, JS (ES6)
-
-You can run this project by running command in CMD/Terminal:
-
-clone https://github.com/leeburnell/BrewdogApiExercise.git
-
+# WorkTest
--- slick/slick.js
+++ slick/slick.js
@@ -3008,4 +3008,52 @@
         return _;
     };
 
+    
+$('.responsive').slick({
+    infinite: true,
+    speed: 300,
+    slidesToShow: 8,
+    slidesToScroll: 1,
+    responsive: [{
+        breakpoint: 1024,
+        settings: {
+            slidesToShow: 6,
+            slidesToScroll: 1,
+            // centerMode: true,
+
+        }
+
+    }, {
+        breakpoint: 800,
+        settings: {
+            slidesToShow: 3,
+            slidesToScroll: 2,
+            dots: true,
+            infinite: true,
+
+        }
+
+
+    }, {
+        breakpoint: 600,
+        settings: {
+            slidesToShow: 2,
+            slidesToScroll: 2,
+            dots: true,
+            infinite: true,
+            
+        }
+    }, {
+        breakpoint: 480,
+        settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1,
+            dots: true,
+            infinite: true,
+            autoplay: true,
+            autoplaySpeed: 2000,
+        }
+    }]
+});
+
 }));
