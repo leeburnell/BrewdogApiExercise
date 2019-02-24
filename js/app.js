4c748bdfc8d0ec5240276376bc7105a3a33b83b9
leeburnell <iambeeroy@gmail.com>
2019-02-24 20:41:39 +0000

    Header change
    Desktop view now replaced by a burger menu

--- js/app.js
+++ js/app.js
@@ -67,3 +67,22 @@ const makeRequest = request('https://api.punkapi.com/v2/beers')
             child.appendChild(paragraph);
         });
     });
+
+    //BurgerMenu.js
+    (function() {
+
+        var hamburger = {
+          navToggle: document.querySelector('.nav-toggle'),
+          nav: document.querySelector('nav'),
+      
+          doToggle: function(e) {
+            e.preventDefault();
+            this.navToggle.classList.toggle('expanded');
+            this.nav.classList.toggle('expanded');
+          }
+        };
+      
+        hamburger.navToggle.addEventListener('click', function(e) { hamburger.doToggle(e); });
+        hamburger.nav.addEventListener('click', function(e) { hamburger.doToggle(e); });
+      
+      }());
